2c3b1a01583eadeec4131476dfd8a5a2a355dce9
Power70 <nwisuanu@gmail.com>
2024-08-24 17:24:27 +0100

    created some new folder and added some files also created the registration page

--- shop/__init__.py
+++ shop/__init__.py
@@ -1,8 +1,17 @@
+# shop/__init__.py
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+
+# set a secrete key
+app.config["SECRET_KEY"] = "b6ee52d0deaf51e633f1b72e0da2cd4c"
+
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///glofarmersMkt.db"
-# create a database instance
+
+# Create a database instance
 db = SQLAlchemy(app)
 
-from shop import routes
+# Import routes after initializing app and db
+from shop.admin import routes
