b5d51202e379f6772d7f48cb1db7ed830f398b82
Power70 <nwisuanu@gmail.com>
2024-09-08 18:18:18 +0100

    edited the addproduct file to  be able the select between a list of brands and category

--- shop/farmsProducts/routes.py
+++ shop/farmsProducts/routes.py
@@ -8,8 +8,9 @@ def addbrand():
     if request.method == 'POST':
         getbrand = request.form.get('brand')
         brand = Brand(name=getbrand)
-        flash(f'The Brand {getbrand} was added to your database', 'success')
+        db.session.add(brand)
         db.session.commit()
+        flash(f'The Brand {getbrand} was added to your database', 'success')
         return redirect(url_for('addbrand'))
     return render_template('products/addbrand.html', brands='brands')
 
@@ -18,13 +19,16 @@ def addcat():
     if request.method == 'POST':
         getbrand = request.form.get('category')
         brand = Category(name=getbrand)
-        flash(f'The Category {getbrand} was added to your database', 'success')
+        db.session.add(brand)
         db.session.commit()
+        flash(f'The Category {getbrand} was added to your database', 'success')
         return redirect(url_for('addcat'))
     return render_template('products/addbrand.html')
 
 
 @app.route('/addproduct', methods=['GET', 'POST'])
 def addproduct():
+    brands = Brand.query.all()
+    categories = Category.query.all()
     form = Addproducts(request.form)
-    return render_template('products/addproduct.html', title="Add product page", form=form)
+    return render_template('products/addproduct.html', title="Add product page", form=form, brands=brands, categories=categories)
